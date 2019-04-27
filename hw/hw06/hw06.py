d304d7b7e38ca3c8cfae88c75be15c84641a033e
Steven (Siwei) Ye <stevenysw@berkeley.edu>
2019-04-27 16:02:50 -0700

    Add files via upload

---
+++ hw/hw06/hw06.py
@@ -0,0 +1,126 @@
+
+passphrase = 'CC74EB'
+
+def survey(p):
+    """
+    You do not need to understand this code.
+    >>> survey(passphrase)
+    '3d2eea56786a3d9e503a4c07dd667867ef3d92bfccd68b2aa0900ead'
+    """
+    import hashlib
+    return hashlib.sha224(p.encode('utf-8')).hexdigest()
+
+
+# Object Oriented Programming
+
+class Fib():
+    """A Fibonacci number.
+
+    >>> start = Fib()
+    >>> start
+    Fib object, value 0
+    >>> start.next()
+    Fib object, value 1
+    >>> start.next().next()
+    Fib object, value 1
+    >>> start.next().next().next()
+    Fib object, value 2
+    >>> start.next().next().next().next()
+    Fib object, value 3
+    >>> start.next().next().next().next().next()
+    Fib object, value 5
+    >>> start.next().next().next().next().next().next()
+    Fib object, value 8
+    >>> start.next().next().next().next().next().next() # Ensure start isn't changed
+    Fib object, value 8
+    """
+
+    def __init__(self, value=0):
+        self.value = value
+
+    def next(self):
+        next_fib = Fib()
+        if self.value == 0:
+            next_fib.pre = 0
+            next_fib.value = 1
+        else:
+            next_fib.pre = self.value
+            next_fib.value = self.value + self.pre
+        return next_fib
+
+    def __repr__(self):
+        return "Fib object, value " + str(self.value)
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Machine is out of stock.'
+    >>> v.deposit(15)
+    'Machine is out of stock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'You must deposit $10 more.'
+    >>> v.deposit(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'You must deposit $3 more.'
+    >>> v.deposit(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.deposit(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.deposit(15)
+    'Machine is out of stock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.deposit(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
+    """
+
+    def __init__(self, product, price):
+        self.product = product
+        self.price = price
+        self.balance = 0
+        self.stock = 0
+
+    def vend(self):
+        if self.stock <= 0:
+            return 'Machine is out of stock.'
+        else:
+            change = self.balance - self.price
+            if change < 0:
+                return 'You must deposit ${0} more.'.format(-change)
+            elif change > 0:
+                self.stock -= 1
+                self.balance = 0
+                return 'Here is your {0} and ${1} change.'.format(self.product, change)
+            else:
+                self.stock -= 1
+                return 'Here is your {0}.'.format(self.product)
+
+    def deposit(self, amount):
+        if self.stock <= 0:
+            return 'Machine is out of stock. Here is your ${0}.'.format(amount)
+        else:
+            self.balance += amount
+            return 'Current balance: ${0}'.format(self.balance)
+
+    def restock(self, stock):
+        self.stock += stock
+        return 'Current {0} stock: {1}'.format(self.product, self.stock)
+            
+            
+            
+            
